b6874d187e196074b4c8ea8ea1f16c4612e146bc
Josh Mize <jgmize@gmail.com>
2018-07-25 10:36:25 -0500

    Test www.allizom.org

--- jenkins/test-only.groovy
+++ jenkins/test-only.groovy
@@ -7,48 +7,29 @@ env.GIT_COMMIT = "latest"
 if ( config.apps ) {
     milestone()
     tested_apps = []
-    // default to oregon-b only
-    def regions = config.regions ?: ['oregon-b']
-    for (regionId in regions) {
-        def region = global_config.regions[regionId]
-        for (appname in config.apps) {
-            if ( config.demo ) {
-                appURL = utils.demoAppURL(appname, region)
-            } else {
-                appURL = "https://${appname}.${region.name}.moz.works"
-            }
-            if ( config.demo ) {
-                appURL = utils.demoAppURL(appname, region)
-            } else {
-                appURL = "https://${appname}.${region.name}.moz.works"
-            }
-            stageName = "Deploy ${appname}-${region.name}"
-            // ensure no deploy/test cycle happens in parallel for an app/region
-            lock (stageName) {
-                if ( config.integration_tests ) {
-                    // queue up test closures
-                    def allTests = [:]
-                    def regionTests = config.integration_tests[regionId]
-                    for (filename in regionTests) {
-                        allTests[filename] = utils.integrationTestJob(filename, appURL)
-                    }
-                    stage ("Test ${appname}-${region.name}") {
-                        try {
-                            // wait for server to be ready
-                            sleep(time: 10, unit: 'SECONDS')
-                            if ( allTests.size() == 1 ) {
-                                allTests[regionTests[0]]()
-                            } else {
-                                parallel allTests
-                            }
-                        } catch(err) {
-                            utils.ircNotification([stage: "Integration Tests ${appname}-${region.name}", status: 'failure'])
-                            throw err
-                        }
-                        tested_apps << "${appname}-${region.name}".toString()
-                    }
+    appURL = config.app_url
+    stageName = "test ${appURL}"
+    if ( config.integration_tests ) {
+        // queue up test closures
+        def allTests = [:]
+        def tests = config.integration_tests
+        for (filename in tests) {
+            allTests[filename] = utils.integrationTestJob(filename, appURL)
+        }
+        stage ("Test ${appURL}") {
+            try {
+                // wait for server to be ready
+                sleep(time: 10, unit: 'SECONDS')
+                if ( allTests.size() == 1 ) {
+                    allTests[tests[0]]()
+                } else {
+                    parallel allTests
                 }
+            } catch(err) {
+                utils.ircNotification([stage: "Integration Tests ${appURL}", status: 'failure'])
+                throw err
             }
+            tested_apps << "${appURL}".toString()
         }
     }
 }
