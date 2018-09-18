960c837bdd48d312fe8e0b24731bf2ddfa3e860f
Josh Mize <jgmize@gmail.com>
2018-09-18 02:08:13 -0500

    Test https://www-akamai.cdn.mozilla.net

--- jenkins/test-only.groovy
+++ jenkins/test-only.groovy
@@ -12,41 +12,26 @@ if ( config.apps ) {
     for (regionId in regions) {
         def region = global_config.regions[regionId]
         for (appname in config.apps) {
-            if ( config.demo ) {
-                appURL = utils.demoAppURL(appname, region)
-            } else {
-                appURL = "https://${appname}.${region.name}.moz.works"
-            }
-            if ( config.demo ) {
-                appURL = utils.demoAppURL(appname, region)
-            } else {
-                appURL = "https://${appname}.${region.name}.moz.works"
-            }
-            stageName = "Deploy ${appname}-${region.name}"
-            // ensure no deploy/test cycle happens in parallel for an app/region
-            lock (stageName) {
-                if ( config.integration_tests ) {
-                    // queue up test closures
-                    def allTests = [:]
-                    def regionTests = config.integration_tests[regionId]
-                    for (filename in regionTests) {
-                        allTests[filename] = utils.integrationTestJob(filename, appURL)
-                    }
-                    stage ("Test ${appname}-${region.name}") {
-                        try {
-                            // wait for server to be ready
-                            sleep(time: 10, unit: 'SECONDS')
-                            if ( allTests.size() == 1 ) {
-                                allTests[regionTests[0]]()
-                            } else {
-                                parallel allTests
-                            }
-                        } catch(err) {
-                            utils.slackNotification([stage: "Integration Tests ${appname}-${region.name}", status: 'failure'])
-                            throw err
+            appURL = "https://www-akamai.cdn.mozilla.net"
+            if ( config.integration_tests ) {
+                // queue up test closures
+                def allTests = [:]
+                def regionTests = config.integration_tests[regionId]
+                for (filename in regionTests) {
+                    allTests[filename] = utils.integrationTestJob(filename, appURL)
+                }
+                stage ("Test ${appURL}") {
+                    try {
+                        if ( allTests.size() == 1 ) {
+                            allTests[regionTests[0]]()
+                        } else {
+                            parallel allTests
                         }
-                        tested_apps << "${appname}-${region.name}".toString()
+                    } catch(err) {
+                        utils.slackNotification([stage: "Integration Tests ${appURL}", status: 'failure'])
+                        throw err
                     }
+                    tested_apps << "${appURL}".toString()
                 }
             }
         }
